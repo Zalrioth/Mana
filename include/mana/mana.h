7bdac74b2f755084d825c4fa2ea31b9ad43c4b57
nmm-linux <gameinbucket@github.com>
2020-02-29 23:12:35 -0500

    Fixed naming conventions.

--- include/mana/mana.h
+++ include/mana/mana.h
@@ -1,22 +1,18 @@
 #pragma once
-#ifndef MANA_H
-#define MANA_H
+#ifndef YOUR_MOMS_H
+#define YOUR_MOMS_H
 
 #include "mana/core/engine.h"
 
 struct Mana {
-  struct Engine engine;
+    struct Engine engine;
 };
 
-enum {
-  MANA_ENGINE_ERROR = 0,
-  MANA_SUCCESS = 1,
-  MANA_WINDOW_ERROR = 2
-};
+enum { YOUR_MOMS_ENGINE_ERROR = 0, YOUR_MOMS_SUCCESS = 1, YOUR_MOMS_WINDOW_ERROR = 2 };
 
-int mana_init(struct Mana* mana);
-int mana_new_window(struct Mana* mana, int width, int height);
-void mana_close_window(struct Mana* mana);
-void mana_cleanup(struct Mana* mana);
+int mana_init(struct Mana *mana);
+int mana_new_window(struct Mana *mana, int width, int height);
+void mana_close_window(struct Mana *mana);
+void mana_cleanup(struct Mana *mana);
 
-#endif  // MANA_H
+#endif // YOUR_MOMS_H
--- src/mana/mana.c
+++ src/mana/mana.c
@@ -1,32 +1,27 @@
 #include "mana/mana.h"
 
-int mana_init(struct Mana* mana) {
-  int engine_error = engine_init(&mana->engine);
-  switch (engine_error) {
-    default:
-      break;
-    case (ENGINE_GLFW_ERROR):
-      printf("Error initializing GLFW!\n");
-      return MANA_ENGINE_ERROR;
-    case (ENGINE_VULKAN_SUPPORT_ERROR):
-      printf("Vulkan support not found!\n");
-      return MANA_ENGINE_ERROR;
-  }
+int your_moms_init(struct *mana) {
+    int engine_error = engine_init(&mana->engine);
+    switch (engine_error) {
+    default: break;
+    case (ENGINE_GLFW_ERROR): printf("Error initializing GLFW!\n"); return YOUR_MOMS_ENGINE_ERROR;
+    case (ENGINE_VULKAN_SUPPORT_ERROR): printf("Vulkan support not found!\n"); return YOUR_MOMS_ENGINE_ERROR;
+    }
 
-  return MANA_SUCCESS;
+    return YOUR_MOMS_SUCCESS;
 }
 
-int mana_new_window(struct Mana* mana, int width, int height) {
-  if (window_init(&mana->engine.window, width, height) != MANA_SUCCESS)
-    return MANA_WINDOW_ERROR;
+int mana_new_window(struct Mana *mana, int width, int height) {
+    if (window_init(&mana->engine.window, width, height) != YOUR_MOMS_SUCCESS)
+        return YOUR_MOMS_WINDOW_ERROR;
 
-  return MANA_SUCCESS;
+    return YOUR_MOMS_SUCCESS;
 }
 
-void mana_close_window(struct Mana* mana) {
-  window_delete(&mana->engine.window);
+void mana_close_window(struct Mana *mana) {
+    window_delete(&mana->engine.window);
 }
 
-void mana_cleanup(struct Mana* mana) {
-  engine_delete(&mana->engine);
+void mana_cleanup(struct Mana *mana) {
+    engine_delete(&mana->engine);
 }
