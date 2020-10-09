66b841d5d3972d523ed8b2c4c5c6f3e0c480c564
Zalrioth <nicholasbedner@gmail.com>
2020-10-09 05:44:30 -0700

    Test build

--- include/mana/core/memoryallocator.h
+++ include/mana/core/memoryallocator.h
@@ -2,26 +2,30 @@
 #ifndef MEMORY_ALLOCATOR_H
 #define MEMORY_ALLOCATOR_H
 
-// TODO: Include header that checks for platform
-// TODO: Check with preprocessor if these can be moved outside or redefined easily
-
-#define CHECK_MEMORY_LEAKS
-#ifdef CHECK_MEMORY_LEAKS  // TODO: And windows
+// Override c std lib memory allocator
+#if CHECK_MEMORY_LEAKS
+#if defined(_WIN32) || defined(__WIN32__) || defined(__WINDOWS__)
 #define _CRTDBG_MAP_ALLOC
 #include <stdlib.h>
 //
 #include <crtdbg.h>
 //
 #include <string.h>
+// TODO: Add memory check for non windows
 #else
 #include <stdlib.h>
 #include <string.h>
 #endif
-
-// Override c std lib memory allocator
-//#define CUSTOM_ALLOCATOR
-#ifdef CUSTOM_ALLOCATOR
+#else
+#if CUSTOM_ALLOCATOR
+#include <stdlib.h>
+#include <string.h>
+//
 #include <mimalloc-override.h>
+#else
+#include <stdlib.h>
+#include <string.h>
+#endif
 #endif
 
 #endif  // MEMORY_ALLOCATOR_H
