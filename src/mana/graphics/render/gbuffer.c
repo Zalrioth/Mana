3e350870dc892c749bbf0a85e354209434d6884f
nickbedner <2321769+nickbedner@users.noreply.github.com>
2021-03-03 01:13:09 -0800

    Vulkan MSAA support

--- src/mana/graphics/render/gbuffer.c
+++ src/mana/graphics/render/gbuffer.c
@@ -1,8 +1,16 @@
 #include "mana/graphics/render/gbuffer.h"
 
+// TODO: Clean up and let user select 8 bit, 10 bit, hdr
+// TODO: Implement sample shading
+// https://vulkan-tutorial.com/Multisampling
 int gbuffer_init(struct GBuffer* gbuffer, struct VulkanState* vulkan_renderer) {
+  // Note: For standard rendering
+  //VK_FORMAT_R8G8B8A8_SRGB
+  // Note: For 10 bit rendering
   //VK_FORMAT_A2B10G10R10_UNORM_PACK32
-  VkFormat image_format = VK_FORMAT_R16G16B16A16_SFLOAT;
+  // Note: For hdr rendering
+  //VkFormat image_format = VK_FORMAT_R16G16B16A16_SFLOAT;
+  VkFormat image_format = VK_FORMAT_R8G8B8A8_SRGB;
   VkImageUsageFlags image_usage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
   VkFormat depth_format = graphics_utils_find_depth_format(vulkan_renderer->physical_device);
 
@@ -19,7 +27,7 @@ int gbuffer_init(struct GBuffer* gbuffer, struct VulkanState* vulkan_renderer) {
 
   graphics_utils_create_image(vulkan_renderer->device, vulkan_renderer->physical_device, gbuffer_width, gbuffer_height, 1, VK_SAMPLE_COUNT_1_BIT, image_format, VK_IMAGE_TILING_OPTIMAL, image_usage, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, &gbuffer->normal_image, &gbuffer->normal_image_memory);
   graphics_utils_create_image_view(vulkan_renderer->device, gbuffer->normal_image, image_format, VK_IMAGE_ASPECT_COLOR_BIT, 1, &vulkan_renderer->gbuffer->normal_image_view);
-  // TODO: Rename depth is not multisampled
+
   graphics_utils_create_image(vulkan_renderer->device, vulkan_renderer->physical_device, gbuffer_width, gbuffer_height, 1, vulkan_renderer->msaa_samples, depth_format, VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, &gbuffer->depth_image, &gbuffer->depth_image_memory);
   graphics_utils_create_image_view(vulkan_renderer->device, gbuffer->depth_image, depth_format, VK_IMAGE_ASPECT_DEPTH_BIT, 1, &gbuffer->depth_image_view);
 
@@ -47,7 +55,6 @@ int gbuffer_init(struct GBuffer* gbuffer, struct VulkanState* vulkan_renderer) {
   VkAttachmentReference depth_attachment_ref = {0};
   depth_attachment_ref.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
 
-  VkFramebufferCreateInfo framebuffer_info = {0};
   if (vulkan_renderer->msaa_samples != 1) {
     color_attachment_ref.attachment = 3;
     normal_attachment_ref.attachment = 4;
@@ -122,6 +129,7 @@ int gbuffer_init(struct GBuffer* gbuffer, struct VulkanState* vulkan_renderer) {
     if (vkCreateRenderPass(vulkan_renderer->device, &render_pass_info, NULL, &gbuffer->render_pass) != VK_SUCCESS)
       return 0;
 
+    VkFramebufferCreateInfo framebuffer_info = {0};
     VkImageView attachments_framebuffer[] = {gbuffer->multisample_color_image_view, gbuffer->multisample_normal_image_view, gbuffer->depth_image_view, gbuffer->color_image_view, gbuffer->normal_image_view};
     framebuffer_info.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
     framebuffer_info.renderPass = gbuffer->render_pass;
@@ -130,21 +138,41 @@ int gbuffer_init(struct GBuffer* gbuffer, struct VulkanState* vulkan_renderer) {
     framebuffer_info.width = gbuffer_width;
     framebuffer_info.height = gbuffer_height;
     framebuffer_info.layers = 1;
+
+    if (vkCreateFramebuffer(vulkan_renderer->device, &framebuffer_info, NULL, &gbuffer->gbuffer_framebuffer) != VK_SUCCESS)
+      return VULKAN_RENDERER_CREATE_FRAME_BUFFER_ERROR;
+
+    VkSemaphoreCreateInfo semaphore_info = {0};
+    semaphore_info.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
+
+    vkCreateSemaphore(vulkan_renderer->device, &semaphore_info, NULL, &gbuffer->gbuffer_semaphore);
+    graphics_utils_create_sampler(vulkan_renderer->device, &gbuffer->texture_sampler, (struct SamplerSettings){.mip_levels = 0, .filter = VK_FILTER_LINEAR, .address_mode = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE});
+
+    gbuffer->projection_matrix = MAT4_ZERO;
+    gbuffer->view_matrix = MAT4_ZERO;
+
+    // Gbuffer command buffer
+    VkCommandBufferAllocateInfo alloc_info_offscreen = {0};
+    alloc_info_offscreen.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    alloc_info_offscreen.commandPool = vulkan_renderer->command_pool;
+    alloc_info_offscreen.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    alloc_info_offscreen.commandBufferCount = 1;
+
+    if (vkAllocateCommandBuffers(vulkan_renderer->device, &alloc_info_offscreen, &gbuffer->gbuffer_command_buffer) != VK_SUCCESS)
+      return VULKAN_RENDERER_CREATE_COMMAND_BUFFER_ERROR;
   } else {
     color_attachment_ref.attachment = 0;
     normal_attachment_ref.attachment = 1;
     depth_attachment_ref.attachment = 2;
 
-    struct VkAttachmentReference color_attachment_references[2] = {color_attachment_ref, normal_attachment_ref};
+    struct VkAttachmentReference color_attachment_references[GBUFFER_COLOR_ATTACHMENTS] = {color_attachment_ref, normal_attachment_ref};
     VkSubpassDescription subpass = {0};
     subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
-    subpass.colorAttachmentCount = 2;
+    subpass.colorAttachmentCount = GBUFFER_COLOR_ATTACHMENTS;
     subpass.pColorAttachments = color_attachment_references;
     subpass.pDepthStencilAttachment = &depth_attachment_ref;
 
-    const int total_dependencies = 2;
-    VkSubpassDependency dependencies[total_dependencies];
-
+    VkSubpassDependency dependencies[GBUFFER_TOTAL_DEPENDENCIES];
     dependencies[0].srcSubpass = VK_SUBPASS_EXTERNAL;
     dependencies[0].dstSubpass = 0;
     dependencies[0].srcStageMask = VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT;
@@ -161,20 +189,20 @@ int gbuffer_init(struct GBuffer* gbuffer, struct VulkanState* vulkan_renderer) {
     dependencies[1].dstAccessMask = VK_ACCESS_MEMORY_READ_BIT;
     dependencies[1].dependencyFlags = VK_DEPENDENCY_BY_REGION_BIT;
 
-    const int total_attachments = 3;
-    VkAttachmentDescription attachments_render_pass[total_attachments] = {color_attachment, normal_attachment, depth_attachment};
+    VkAttachmentDescription attachments_render_pass[GBUFFER_TOTAL_ATTACHMENTS] = {color_attachment, normal_attachment, depth_attachment};
     VkRenderPassCreateInfo render_pass_info = {0};
     render_pass_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
     render_pass_info.pAttachments = attachments_render_pass;
-    render_pass_info.attachmentCount = total_attachments;
+    render_pass_info.attachmentCount = GBUFFER_TOTAL_ATTACHMENTS;
     render_pass_info.subpassCount = 1;
     render_pass_info.pSubpasses = &subpass;
-    render_pass_info.dependencyCount = total_dependencies;
+    render_pass_info.dependencyCount = GBUFFER_TOTAL_DEPENDENCIES;
     render_pass_info.pDependencies = dependencies;
 
     if (vkCreateRenderPass(vulkan_renderer->device, &render_pass_info, NULL, &gbuffer->render_pass) != VK_SUCCESS)
       return 0;
 
+    VkFramebufferCreateInfo framebuffer_info = {0};
     VkImageView attachments_framebuffer[] = {gbuffer->color_image_view, gbuffer->normal_image_view, gbuffer->depth_image_view};
     framebuffer_info.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
     framebuffer_info.renderPass = gbuffer->render_pass;
@@ -183,29 +211,29 @@ int gbuffer_init(struct GBuffer* gbuffer, struct VulkanState* vulkan_renderer) {
     framebuffer_info.width = gbuffer_width;
     framebuffer_info.height = gbuffer_height;
     framebuffer_info.layers = 1;
+
+    if (vkCreateFramebuffer(vulkan_renderer->device, &framebuffer_info, NULL, &gbuffer->gbuffer_framebuffer) != VK_SUCCESS)
+      return VULKAN_RENDERER_CREATE_FRAME_BUFFER_ERROR;
+
+    VkSemaphoreCreateInfo semaphore_info = {0};
+    semaphore_info.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
+
+    vkCreateSemaphore(vulkan_renderer->device, &semaphore_info, NULL, &gbuffer->gbuffer_semaphore);
+    graphics_utils_create_sampler(vulkan_renderer->device, &gbuffer->texture_sampler, (struct SamplerSettings){.mip_levels = 0, .filter = VK_FILTER_LINEAR, .address_mode = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE});
+
+    gbuffer->projection_matrix = MAT4_ZERO;
+    gbuffer->view_matrix = MAT4_ZERO;
+
+    // Gbuffer command buffer
+    VkCommandBufferAllocateInfo alloc_info_offscreen = {0};
+    alloc_info_offscreen.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    alloc_info_offscreen.commandPool = vulkan_renderer->command_pool;
+    alloc_info_offscreen.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    alloc_info_offscreen.commandBufferCount = 1;
+
+    if (vkAllocateCommandBuffers(vulkan_renderer->device, &alloc_info_offscreen, &gbuffer->gbuffer_command_buffer) != VK_SUCCESS)
+      return VULKAN_RENDERER_CREATE_COMMAND_BUFFER_ERROR;
   }
-
-  if (vkCreateFramebuffer(vulkan_renderer->device, &framebuffer_info, NULL, &gbuffer->gbuffer_framebuffer) != VK_SUCCESS)
-    return VULKAN_RENDERER_CREATE_FRAME_BUFFER_ERROR;
-
-  VkSemaphoreCreateInfo semaphore_info = {0};
-  semaphore_info.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
-
-  vkCreateSemaphore(vulkan_renderer->device, &semaphore_info, NULL, &gbuffer->gbuffer_semaphore);
-  graphics_utils_create_sampler(vulkan_renderer->device, &gbuffer->texture_sampler, (struct SamplerSettings){.mip_levels = 0, .filter = VK_FILTER_LINEAR, .address_mode = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE});
-
-  gbuffer->projection_matrix = MAT4_ZERO;
-  gbuffer->view_matrix = MAT4_ZERO;
-
-  // Gbuffer command buffer
-  VkCommandBufferAllocateInfo alloc_info_offscreen = {0};
-  alloc_info_offscreen.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
-  alloc_info_offscreen.commandPool = vulkan_renderer->command_pool;
-  alloc_info_offscreen.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-  alloc_info_offscreen.commandBufferCount = 1;
-
-  if (vkAllocateCommandBuffers(vulkan_renderer->device, &alloc_info_offscreen, &gbuffer->gbuffer_command_buffer) != VK_SUCCESS)
-    return VULKAN_RENDERER_CREATE_COMMAND_BUFFER_ERROR;
 
   return 1;
   //return VULKAN_RENDERER_SUCCESS;
@@ -260,8 +288,7 @@ int gbuffer_start(struct GBuffer* gbuffer, struct VulkanState* vulkan_renderer) 
   render_pass_info.renderArea.extent.height = vulkan_renderer->swap_chain->swap_chain_extent.height * vulkan_renderer->swap_chain->supersample_scale;
 
   if (vulkan_renderer->msaa_samples != 1) {
-    VkClearValue clear_values[MULTISAMPLE_GBUFFER_TOTAL_ATTACHMENTS];
-    memset(clear_values, 0, sizeof(clear_values));
+    VkClearValue clear_values[MULTISAMPLE_GBUFFER_TOTAL_ATTACHMENTS] = {0};
 
     // Should not be clearing color only normals and depth
     // Faster to just draw over old color content instead of clearing
@@ -285,9 +312,10 @@ int gbuffer_start(struct GBuffer* gbuffer, struct VulkanState* vulkan_renderer) 
 
     render_pass_info.clearValueCount = MULTISAMPLE_GBUFFER_TOTAL_ATTACHMENTS;
     render_pass_info.pClearValues = clear_values;
+
+    vkCmdBeginRenderPass(gbuffer->gbuffer_command_buffer, &render_pass_info, VK_SUBPASS_CONTENTS_INLINE);
   } else {
-    VkClearValue clear_values[GBUFFER_TOTAL_ATTACHMENTS];
-    memset(clear_values, 0, sizeof(clear_values));
+    VkClearValue clear_values[GBUFFER_TOTAL_ATTACHMENTS] = {0};
 
     //VkClearColorValue clear_color = {{0.0f, 0.0f, 0.0f, 0.0f}};
     VkClearColorValue clear_color = {{0.529f, 0.808f, 0.922f, 0.0f}};
@@ -301,9 +329,9 @@ int gbuffer_start(struct GBuffer* gbuffer, struct VulkanState* vulkan_renderer) 
 
     render_pass_info.clearValueCount = GBUFFER_TOTAL_ATTACHMENTS;
     render_pass_info.pClearValues = clear_values;
-  }
 
-  vkCmdBeginRenderPass(gbuffer->gbuffer_command_buffer, &render_pass_info, VK_SUBPASS_CONTENTS_INLINE);
+    vkCmdBeginRenderPass(gbuffer->gbuffer_command_buffer, &render_pass_info, VK_SUBPASS_CONTENTS_INLINE);
+  }
 
   return VULKAN_RENDERER_SUCCESS;
 }
